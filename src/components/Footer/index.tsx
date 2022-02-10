a17b4519211a8c47ba012e0dc5d79ea2689a9d64
Igor Santos <igorsantoscosta@gmail.com>
2022-02-10 20:22:02 -0300

    Edit layout index page #5

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,10 @@ import styles from './styles.module.scss'
 export const Footer = () => {
   return (
     <div className={styles.container}>
-      &copy; 2021 - Santa Luzia Fretamento e Turismo
+      &copy; 2022 - Santa Luzia Fretamento e Turismo
       <br />
       <br />
-      Criado e desenvolvido por{' '}
+      Desenvolvido por{' '}
       <a href="http://www.igsdesign.com.br" target="_blank" rel="noreferrer">
         <b>IGS DESIGN</b>
       </a>
